ca478db2e0705b646e7f048493ddb7e31cae21a8
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2021-07-05 00:32:20 +0300

    web: minor changes.

--- web/src/views/Jobs.tsx
+++ web/src/views/Jobs.tsx
@@ -1,10 +1,18 @@
-import React, {FunctionComponent} from "react";
+import React, {FunctionComponent, PropsWithChildren} from "react";
 import {EmptyState} from "../components";
-import {Box} from "@material-ui/core";
+import {Box, Button} from "@material-ui/core";
 
 import Icon from "@material-ui/icons/AccountTree";
+import ArrowRigthIcon from '@material-ui/icons/SubdirectoryArrowRight';
 
-const Jobs: FunctionComponent = (props) => {
+const Jobs: FunctionComponent = (props: PropsWithChildren<any>) => {
+    const actionButton = (
+        <Button
+            variant={"outlined"}
+            startIcon={<ArrowRigthIcon/>}>
+            Go to Task Force
+        </Button>
+    );
 
     return (
         <Box width={"100%"} height={"100%"}>
@@ -12,6 +20,7 @@ const Jobs: FunctionComponent = (props) => {
                         title={"No Jobs Found"}
                         subTitle={"Your assistant shows you fun new things automatically\n" +
                         "ceated from your photos and helps you to say organised"}
+                        actionButton={actionButton}
             />
         </Box>
     )
