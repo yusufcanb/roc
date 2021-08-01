027ff7ad97d1eca12aa5e47c6d1f79572356d0bd
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2021-08-02 00:28:57 +0300

    web: project create view added

--- web/src/project/views/ProjectListView.tsx
+++ web/src/project/views/ProjectListView.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {FunctionComponent} from 'react';
 import PageContent from "../../core/components/PageContent";
 import {useStore} from "../../core/store";
 import {Button} from "@material-ui/core";
 import ProjectList from "../components/ProjectList";
+import {useHistory} from "react-router-dom";
+import {Add} from "@material-ui/icons";
 
 
-const ProjectListView = () => {
+const ProjectListView: FunctionComponent = () => {
     const {projectStore} = useStore();
+    const history = useHistory();
 
     return (
-        <PageContent right={<Button variant={"contained"} color={"secondary"}>Create Project</Button>}>
+        <PageContent
+            right={<Button onClick={() => history.push("/projects/new")} startIcon={<Add/>} variant={"contained"}
+                           color={"secondary"}>Create
+                Project</Button>}>
             <ProjectList projects={projectStore.projects}/>
         </PageContent>
     );
