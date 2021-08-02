87b7391aecaa6f2aeede2120cb24c0d1fba9da35
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2021-08-02 17:20:39 +0300

    web: eslint fix

--- web/src/task-force/components/TaskForceCreate.tsx
+++ web/src/task-force/components/TaskForceCreate.tsx
@@ -2,6 +2,7 @@ import React, {FunctionComponent, PropsWithChildren, useState} from "react";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid, TextField} from "@material-ui/core";
 import RobotTransferList from "./RobotTransferList";
+import {ImportExport} from "@material-ui/icons";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -25,6 +26,10 @@ const useStyles = makeStyles((theme: Theme) =>
             margin: theme.spacing(1),
             minWidth: 120,
         },
+        robotSelectButton: {
+            height: "calc(100% - 22px)",
+            float: "right"
+        }
     }),
 );
 
@@ -33,15 +38,14 @@ interface TaskForceCreateProps {
 }
 
 const TaskForceCreate: FunctionComponent<TaskForceCreateProps> = (props: PropsWithChildren<TaskForceCreateProps>) => {
+    const variant: any = "filled";
     const classes = useStyles();
 
-    const [name, setName] = useState<string>("");
+    const [, setName] = useState<string>("");
     const [open, setOpen] = React.useState(false);
-    const [age, setAge] = React.useState<number | string>('');
 
-    const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-        setAge(Number(event.target.value) || '');
-    };
+    // const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    // };
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -50,7 +54,6 @@ const TaskForceCreate: FunctionComponent<TaskForceCreateProps> = (props: PropsWi
     const handleClose = () => {
         setOpen(false);
     };
-    const variant: any = "filled";
 
     return <Grid
         container
@@ -84,22 +87,36 @@ const TaskForceCreate: FunctionComponent<TaskForceCreateProps> = (props: PropsWi
             />
         </Grid>
         <Grid className={classes.item} item>
-            <Button variant={variant} size={"large"} className={classes.textField} onClick={handleClickOpen}>Select
-                Robots</Button>
-            <Dialog open={open} onClose={handleClose} maxWidth={"md"} fullWidth={true}>
-                <DialogTitle>Assign Robots</DialogTitle>
-                <DialogContent className={classes.dialogContainer}>
-                    <RobotTransferList/>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose} color="primary">
-                        Cancel
+            <Grid container justify={"space-between"}>
+                <Grid item md={10}>
+                    <TextField className={classes.textField}
+                               disabled={true}
+                               variant={variant}
+                               label="Assigned Robots"
+                               helperText="Assign project's robots to this task force."
+                    />
+                </Grid>
+                <Grid item md={1}>
+                    <Button startIcon={<ImportExport />} className={classes.robotSelectButton} color={"primary"} variant={"outlined"}
+                            onClick={handleClickOpen}>
+                        Select
                     </Button>
-                    <Button onClick={handleClose} color="primary">
-                        Ok
-                    </Button>
-                </DialogActions>
-            </Dialog>
+                    <Dialog open={open} onClose={handleClose} maxWidth={"md"} fullWidth={true}>
+                        <DialogTitle>Assign Robots</DialogTitle>
+                        <DialogContent className={classes.dialogContainer}>
+                            <RobotTransferList/>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Cancel
+                            </Button>
+                            <Button onClick={handleClose} color="primary">
+                                Ok
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </Grid>
+            </Grid>
         </Grid>
 
     </Grid>
