f0b3f4bace9ba3d5948750b7f7e5164861852324
Yusuf Can Bayrak <yusuf.can.bayrak@huawei.com>
2021-10-03 03:14:21 +0300

    agent: early implementations

--- agent/src/main/java/org/robotframework/roc/agent/ws/ClientStompSessionHandler.java
+++ agent/src/main/java/org/robotframework/roc/agent/ws/ClientStompSessionHandler.java
@@ -3,6 +3,7 @@ package org.robotframework.roc.agent.ws;
 import lombok.extern.slf4j.Slf4j;
 import org.robotframework.roc.agent.EventQueue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
@@ -15,10 +16,13 @@ public class ClientStompSessionHandler extends StompSessionHandlerAdapter {
     @Autowired
     EventQueue queue;
 
+    @Value("${roc.agent.id}")
+    private String agentId;
+
     @Override
     public void afterConnected(StompSession session, StompHeaders headers) {
         log.info("Client connected: headers {}", headers);
-        session.subscribe("/queue/busybox", this);
+        session.subscribe(String.format("/queue/events.%s", this.agentId), this);
     }
 
     @Override
