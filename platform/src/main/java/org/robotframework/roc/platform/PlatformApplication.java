55b7fee74765756805df645e6cad1c6145eae0cc
Yusuf Can Bayrak <yusuf.can.bayrak@huawei.com>
2021-12-02 01:37:47 +0300

    platform: create a default project if not exists on boot

--- platform/src/main/java/org/robotframework/roc/platform/PlatformApplication.java
+++ platform/src/main/java/org/robotframework/roc/platform/PlatformApplication.java
@@ -1,18 +1,41 @@
 package org.robotframework.roc.platform;
 
+import lombok.extern.slf4j.Slf4j;
+import org.robotframework.roc.core.models.Project;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import org.robotframework.roc.core.services.ProjectService;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EntityScan("org.robotframework.roc.*")
 @ComponentScan("org.robotframework.roc.*")
+@Slf4j
 public class PlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlatformApplication.class);
+    }
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return (String[] args) -> {
+            ProjectService service = ctx.getBean(ProjectService.class);
+            if (!(service.getProjects().size() > 0)) {
+                log.info("No project exists.. creating a default project");
+                Project p = new Project();
+                p.setName("Default Project");
+                p.setDefault(true);
+                service.createProject(p);
+            }
+        };
     }
 
 }
--- platform/src/main/java/org/robotframework/roc/platform/project/service/ProjectServiceImpl.java
+++ platform/src/main/java/org/robotframework/roc/platform/project/service/ProjectServiceImpl.java
@@ -1,13 +1,14 @@
 package org.robotframework.roc.platform.project.service;
 
 import org.robotframework.roc.core.models.Project;
-import org.robotframework.roc.core.services.FileService;
 import org.robotframework.roc.core.services.GlobalVariableService;
 import org.robotframework.roc.core.services.ProjectService;
 import org.robotframework.roc.platform.project.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.Collection;
+import java.util.Date;
 import java.util.Optional;
 
 @Service
@@ -26,6 +27,7 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public Project createProject(Project project) {
+        project.setCreatedAt(Date.from(Instant.now()));
         return projectRepository.save(project);
     }
 
