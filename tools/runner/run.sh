a6664ba428760afa9d7aabf2d433c6c9f3946b4a
Yusuf Can Bayrak <yusuf.bayrak@enocean.com>
2022-07-05 13:43:51 +0200

    rm runner directory

--- tools/runner/run.sh
+++
@@ -1,5 +0,0 @@
-docker run --rm \
-  -v `pwd`/variables.yaml:/opt/robotframework/variables.yaml:Z \
-  -v `pwd`/tasks:/opt/robotframework/tests:Z \
-  -v `pwd`/reports:/opt/robotframework/reports:Z \
-  ghcr.io/yusufcanb/roc-runner:5.0.0-rc.0
--- tools/runner/tasks/demo.robot
+++
@@ -1,6 +0,0 @@
-*** Settings ***
-Documentation       Demo robot for testing purposes.
-
-*** Test Cases ***
-First Task
-    Log    Okey! I'm done.
