e82cbf0b237f7471faca50ad1a168f070f91f012
Yusuf Can Bayrak <yusuf.bayrak@enocean.com>
2022-11-11 23:47:11 +0100

    lint fixes

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    // collectCoverage: true,
-    // coverageDirectory: "coverage",
-    // collectCoverageFrom: [
-    //     "src/**/*.{ts,js,jsx}"
-    // ],
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    collectCoverageFrom: [
+        "src/**/*.{ts,js,jsx}"
+    ],
     coveragePathIgnorePatterns: [
         "jest.config.js",
         "/node_modules/",
--- packages/tunnel/src/index.ts
+++ packages/tunnel/src/index.ts
@@ -16,35 +16,36 @@
  *
  */
 
-const websocket = require("ws")
-const redis = require("redis")
+import { RedisClientType, createClient } from "@redis/client";
+import { Server, WebSocket } from "ws";
 
-// Configuration: adapt to your environment
 const REDIS_SERVER = process.env.REDIS_URL || "redis://localhost:6379";
 
-const TUNNEL_HOST = process.env.TUNNEL_HOST || "0.0.0.0"
+const TUNNEL_HOST = process.env.TUNNEL_HOST || "0.0.0.0";
 const TUNNEL_PORT = parseInt(process.env.TUNNEL_PORT) || 5000;
 
-// Connect to Redis and subscribe to "app:notifications" channel
-const redisClient = redis.createClient({
-    url: REDIS_SERVER
+const redisClient: RedisClientType = createClient({
+  url: REDIS_SERVER,
 });
 
 // Create & Start the WebSocket server
-const server = new websocket.Server({ host: TUNNEL_HOST, port: TUNNEL_PORT });
+const server: Server = new Server({
+  host: TUNNEL_HOST,
+  port: TUNNEL_PORT,
+});
 
-// Register event for client connection
-server.on('connection', async (ws) => {
-    console.log("connected")
+// Register event on client connection
+server.on("connection", async (ws: WebSocket) => {
+  console.log("connected");
 
-    // new redis connection for each websocket connection
-    const subscriber = redisClient.duplicate();
-    await subscriber.connect();
+  // create new redis connection for each websocket connection
+  const subscriber = redisClient.duplicate();
+  await subscriber.connect();
 
-    await subscriber.pSubscribe('*', (message, channel) => {
-        console.log(`New event received -> ${channel}::${message}`)
-        ws.send(`${channel}::${message}`);
-    });
+  await subscriber.pSubscribe("*", (message, channel) => {
+    console.log(`New event received -> ${channel}::${message}`);
+    ws.send(`${channel}::${message}`);
+  });
 });
 
 console.log(`WS Tunnel started at ws://${TUNNEL_HOST}:${TUNNEL_PORT}`);
