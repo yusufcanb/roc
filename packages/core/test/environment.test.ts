1a6c70527cbc9753a3e752d22dbd9cf3d1cf72a4
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2022-11-22 22:16:06 +0000

    test change

---
+++ packages/core/test/environment.test.ts
@@ -0,0 +1,53 @@
+import { Environment, EnvironmentCreateDto } from '@roc/core';
+import { validate } from 'class-validator';
+
+describe('core.environment', () => {
+  describe('Environment', () => {
+    test('::constructor()', () => {
+      var e = new Environment();
+      expect(e).not.toBe(null);
+    });
+    test('::fromPlainObject()', () => {
+      const plainObject = {
+        name: 'test-env',
+        description: 'a test env to test env',
+      };
+      const environment = Environment.fromPlainObject(plainObject);
+
+      expect(environment).not.toBe(null);
+      expect(environment).toBeInstanceOf(Environment);
+
+      expect(environment.name).toBe(plainObject.name);
+      expect(environment.description).toBe(plainObject.description);
+
+      expect(environment.tags).toStrictEqual([]);
+      expect(environment.variables).toStrictEqual({});
+    });
+    test('::toPlainObject()', () => {});
+  });
+
+  describe('EnvironmentCreateDto', () => {
+    test('it validates correctly when given fields are valid', () => {
+      let envDto = new EnvironmentCreateDto();
+      envDto.name = 'valid-env-name';
+      envDto.description = 'hello world!';
+      envDto.yaml = 'not null';
+
+      validate(envDto).then((errors) => {
+        expect(errors.length).toEqual(0);
+      });
+    });
+
+    test('it has validation errors when given name does not match the name convention', () => {
+      let envDto = new EnvironmentCreateDto();
+      envDto.name = 'valid-env_!! ff';
+      envDto.yaml = 'not null';
+      validate(envDto).then((errors) => {
+        expect(errors.length).toEqual(1);
+        expect(errors[0].constraints.matches).toEqual(
+          'name must match /^[a-z0-9-]+$/ regular expression',
+        );
+      });
+    });
+  });
+});
