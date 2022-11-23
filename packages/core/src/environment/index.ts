5943525c14fa609722dfd75b6e042bf6f082911f
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2022-11-23 20:54:21 +0000

    repositories added

--- packages/core/src/environment/index.ts
+++ packages/core/src/environment/index.ts
@@ -1,2 +1,3 @@
 export * from './entity';
 export * from './dto';
+export * from './repository';
---
+++ packages/core/src/project/repository.ts
@@ -0,0 +1,4 @@
+import { Repository } from '../commons';
+import { Project } from './entity';
+
+export type ProjectRepository = Repository<Project>;
