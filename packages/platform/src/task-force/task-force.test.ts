5365c9808940b3ab6030b02a87e5971104defff2
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2022-11-25 23:37:02 +0000

    task force tests

--- packages/platform/src/task-force/task-force.test.ts
+++ packages/platform/src/task-force/task-force.test.ts
@@ -1,16 +1,20 @@
 import { Test } from '@nestjs/testing';
 
 import { RedisClientType } from 'redis';
-import { RedisModule } from '../redis/redis.module';
-import { TaskForceRedisRepository } from './task-force.repository';
+import {
+  TaskForceModule,
+  TaskForceRedisRepository,
+  TaskForceService,
+  TaskForceController,
+} from '.';
 
-describe('ProjectRedisRepository', () => {
+describe('TaskForceRedisRepository', () => {
   let taskForceRepository: TaskForceRedisRepository;
   let redis: RedisClientType;
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [RedisModule],
+      imports: [TaskForceModule],
       providers: [TaskForceRedisRepository],
     }).compile();
 
@@ -27,5 +31,61 @@ describe('ProjectRedisRepository', () => {
 
   it('::constructor()', async () => {
     expect(await taskForceRepository).not.toBeNull();
+  });
+});
+
+describe('TaskForceService', () => {
+  let taskForceService: TaskForceService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [TaskForceModule],
+      providers: [],
+    }).compile();
+
+    taskForceService = moduleRef.get<TaskForceService>(TaskForceService);
+  });
+
+  afterEach(async () => {});
+
+  it('::constructor()', async () => {
+    expect(await taskForceService).not.toBeNull();
+  });
+
+  it('::constructor()', async () => {
+    const mock = jest.spyOn((taskForceService as any).repository, 'findAll');
+    mock.mockImplementation(async (...args) => []);
+
+    expect(await taskForceService.findAll()).toStrictEqual([]);
+  });
+});
+
+describe('TaskForceController', () => {
+  let taskForceController: TaskForceController;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [TaskForceModule],
+      providers: [],
+    }).compile();
+
+    taskForceController =
+      moduleRef.get<TaskForceController>(TaskForceController);
+  });
+
+  afterEach(async () => {});
+
+  it('::constructor()', async () => {
+    expect(await taskForceController).not.toBeNull();
+  });
+
+  it('::constructor()', async () => {
+    const mock = jest.spyOn(
+      (taskForceController as any).taskForceService,
+      'findAll',
+    );
+    mock.mockImplementation(async (...args) => []);
+
+    expect(await taskForceController.getTaskForces()).toStrictEqual([]);
   });
 });
