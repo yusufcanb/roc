1580c5deaeaa9cb977761c808bff5df6fca367d7
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2022-12-16 23:33:16 +0000

    static serve swagger

--- packages/platform/src/main.ts
+++ packages/platform/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const express = require('express');
+const path = require('path');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
+  app.use('/', express.static(path.join(__dirname, 'public')));
+
   await app.listen(3000);
 }
 
