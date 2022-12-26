3efc77091c0bcf75338f9462a5072e78ec5be8ba
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2022-12-26 15:45:05 +0000

    project exists pipe implementation

--- packages/platform/src/project/project.module.ts
+++ packages/platform/src/project/project.module.ts
@@ -6,16 +6,19 @@ import {
   ProjectController,
   ProjectDetailController,
 } from './project.controller';
+import { ProjectExistsPipe } from './project.pipe';
 import { ProjectRedisRepository } from './project.repository';
 
 @Module({
   imports: [RedisModule],
   controllers: [ProjectController, ProjectDetailController],
   providers: [
+    ProjectExistsPipe,
     {
       provide: 'ProjectRepository',
       useClass: ProjectRedisRepository,
     },
   ],
+  exports: [ProjectExistsPipe],
 })
 export class ProjectModule {}
