1d489b57447d4de9d664bf971604cedfbcecf7ef
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2023-01-02 20:43:20 +0000

    initial executor impl

--- packages/executor/jest.config.js
+++ packages/executor/jest.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    // collectCoverage: true,
-    // coverageDirectory: "coverage",
-    // collectCoverageFrom: [
-    //     "src/**/*.{ts,js,jsx}"
-    // ],
-    coveragePathIgnorePatterns: [
-        "jest.config.js",
-        "/node_modules/",
-        "/dist/",
-        "/lib/",
-    ],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testTimeout: 60_000,
+  // collectCoverage: true,
+  // coverageDirectory: "coverage",
+  // collectCoverageFrom: [
+  //     "src/**/*.{ts,js,jsx}"
+  // ],
+  coveragePathIgnorePatterns: [
+    'jest.config.js',
+    '/node_modules/',
+    '/dist/',
+    '/lib/',
+  ],
 };
--- packages/executor/src/index.ts
+++ packages/executor/src/index.ts
@@ -22,17 +22,65 @@ import {
   TaskForce,
 } from '@roc/core';
 import Docker from 'dockerode';
+import { createWriteStream } from 'fs';
 
 export class DockerRobotExecutor implements RobotExecutor {
   private readonly docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
-  private async _exec(container: Docker.Container, cmd: string[]) {
+  private async _cloneTaskForceToContainer(
+    container: Docker.Container,
+    taskForce: TaskForce,
+  ): Promise<Docker.ExecInspectInfo> {
+    const path = `/opt/${taskForce.projectId}/${taskForce.id}`;
+    return await this._exec(container, [
+      'git',
+      'clone',
+      taskForce.repository,
+      path,
+    ]);
+  }
+
+  private async _executeTaskForceInContainer(
+    container: Docker.Container,
+    taskForce: TaskForce,
+  ): Promise<Docker.ExecInspectInfo> {
+    return await this._exec(container, [
+      'sh',
+      '/opt/robotframework/bin/run-tests-in-virtual-screen.sh',
+    ]);
+  }
+
+  private async _uploadArtifactsToMinio(
+    container: Docker.Container,
+    taskForce: TaskForce,
+    config: RobotExecutorConfig,
+  ): Promise<Docker.ExecInspectInfo> {
+    return await this._exec(container, [
+      'aws',
+      `--endpoint-url=${config.minio.endpoint.toString()}`,
+      's3',
+      'sync',
+      '/opt/robotframework/reports',
+      `s3://${config.minio.bucket}/robot-reports/`,
+    ]);
+  }
+
+  private async _exec(
+    container: Docker.Container,
+    cmd: string[],
+  ): Promise<Docker.ExecInspectInfo> {
     const exec = await container.exec({
       Cmd: cmd,
+      AttachStderr: true,
+      AttachStdin: true,
+      AttachStdout: true,
     });
 
-    const stream = await exec.start({});
-    await container.modem.demuxStream(stream, process.stdout, process.stderr);
+    const execStream = await exec.start({});
+
+    const stdout = createWriteStream(`${cmd[0]}-stdout.txt`);
+    const stderr = createWriteStream(`${cmd[0]}-stderr.txt`);
+    await container.modem.demuxStream(execStream, stdout, stderr);
 
     while ((await exec.inspect()).Running) {
       continue;
@@ -48,17 +96,30 @@ export class DockerRobotExecutor implements RobotExecutor {
   ): Promise<void> {
     const container = await this.docker.createContainer({
       Image: taskForce.runner,
+      HostConfig: {
+        NetworkMode: 'host',
+      },
       Env: [
         `AWS_ACCESS_KEY_ID=${config.minio.accessKey}`,
         `AWS_SECRET_ACCESS_KEY=${config.minio.accessSecret}`,
         `AWS_BUCKET_NAME=${config.minio.bucket}`,
+        `ROBOT_TESTS_DIR=/opt/${taskForce.projectId}/${taskForce.id}/${taskForce.selector}`,
       ],
+      Cmd: ['sleep', 'infinity'],
+      User: 'root',
     });
     await container.start();
 
-    const result = await this._exec(container, ['ls']);
-    console.log(result);
+    console.log(await this._cloneTaskForceToContainer(container, taskForce));
+    console.log(await this._executeTaskForceInContainer(container, taskForce));
+    console.log(
+      await this._uploadArtifactsToMinio(container, taskForce, config),
+    );
+
+    await container.stop();
+    await container.remove();
   }
+
   executeMany(
     environment: Environment,
     taskForce: TaskForce,
