bcc4ae3ec0d253852ff370da02f1432e3b7cef43
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2023-01-05 11:49:33 +0000

    common executor interface update

--- packages/core/src/commons/executor.ts
+++ packages/core/src/commons/executor.ts
@@ -1,3 +1,5 @@
+import { Client as MinioClient } from 'minio';
+import { RedisClientType } from 'redis';
 import { Environment } from '../environment';
 import { TaskForce } from '../task-force';
 
@@ -11,12 +13,33 @@ export interface RobotExecutorConfig {
 }
 
 export interface RobotExecutor {
+  /**
+   * Initializes the executor with a Redis client and a Minio client.
+   * @param redis - The Redis client to use.
+   * @param minio - The Minio client to use.
+   */
+  init(redis: RedisClientType, minio: MinioClient);
+
+  /**
+   * Executes a task force in the given environment.
+   * @param environment - The environment in which to execute the task.
+   * @param taskForce - The task force to use for execution.
+   * @param config - An optional configuration object for the execution.
+   * @returns A promise that resolves when the task has been completed.
+   */
   execute(
     environment: Environment,
     taskForce: TaskForce,
     config?: RobotExecutorConfig,
   ): Promise<void>;
 
+  /**
+   * Executes many task forces in the given environment
+   * @param environment - The environment in which to execute the tasks.
+   * @param taskForce - The task force to use for execution.
+   * @param config - An optional configuration object for the execution.
+   * @returns A promise that resolves when all tasks have been completed.
+   */
   executeMany(
     environment: Environment,
     taskForce: TaskForce,
