e66158b86181230d6adc7a02893dff7a68da483f
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2023-01-19 23:40:24 +0000

    minor changes

--- packages/cli/cmd/environment.go
+++ packages/cli/cmd/environment.go
@@ -15,7 +15,7 @@ var environmentCmd = &cobra.Command{
 
 var environmentListCmd = &cobra.Command{
 	Use:   "list",
-	Short: "Environment list",
+	Short: "Get environment list by project",
 	Run: func(cmd *cobra.Command, args []string) {
 		projectId := getProjectFromFlag(cmd)
 		environmentService.GetEnvironmentsByProjectId(projectId)
@@ -24,17 +24,28 @@ var environmentListCmd = &cobra.Command{
 
 var environmentDetailCmd = &cobra.Command{
 	Use:   "get",
-	Short: "Environment details",
+	Short: "Get an environment detail by its id",
 	Run: func(cmd *cobra.Command, args []string) {
 		projectId := getProjectFromFlag(cmd)
 		environmentService.GetEnvironmentById(projectId, args[0])
 	},
 }
 
+var environmentDeleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: "Delete an environment by its id",
+	Run: func(cmd *cobra.Command, args []string) {
+		projectId := getProjectFromFlag(cmd)
+		environmentService.DeleteEnvironmentById(projectId, args[0])
+	},
+}
+
 func init() {
 	environmentCmd.AddCommand(environmentListCmd)
 	environmentCmd.AddCommand(environmentDetailCmd)
+	environmentCmd.AddCommand(environmentDeleteCmd)
 	rootCmd.AddCommand(environmentCmd)
 
 	environmentCmd.PersistentFlags().StringP("project", "p", "", "Project identifier. e.g default-project")
+	environmentCmd.MarkPersistentFlagRequired("project")
 }
