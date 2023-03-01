36443cdb40ed5b0e3d2a88618ad6521bc73341c8
Yusuf Can Bayrak <yusufcanbayrak@gmail.com>
2023-03-01 19:44:28 +0000

    lint fix

--- packages/executor/.eslintrc.js
+++ packages/executor/.eslintrc.js
@@ -15,8 +15,9 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '*.test.ts'],
   rules: {
+    '@typescript-eslint/no-unused-vars': ['off', { 'argsIgnorePattern': '^_' }],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
